c9d5228e16aa4c40aa9dd8e15989245fd5e3cb6e
mboufadene05-lgtm <mboufadene05@gmail.com>
2026-03-14 11:07:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } 
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } 
 from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
 const firebaseConfig = {
@@ -13,41 +13,41 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+// Fonction de connexion
 window.login = async function() {
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
-    await signInWithEmailAndPassword(auth, email, password);
-    alert("Connexion réussie !");
-  } catch (error) {
-    alert(error.message);
-  }
-};
 
-window.login = async function() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  try {
     await signInWithEmailAndPassword(auth, email, password);
+
     alert("Connexion réussie");
-  } catch (error) {
-    alert(error.message);
-  }
-};
-import { onAuthStateChanged } 
-from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
+    // redirection vers la page du cahier
+    window.location.href = "dashboard.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+};
+
+
+// Vérifie si utilisateur connecté
 onAuthStateChanged(auth, (user) => {
 
   if (user) {
 
-    console.log("Utilisateur connecté :", user.email)
+    console.log("Utilisateur connecté :", user.email);
 
   } else {
 
-    window.location.href = "index.html"
+    window.location.href = "index.html";
 
   }
 
